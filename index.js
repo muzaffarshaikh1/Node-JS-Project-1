616bd89ad582e91ea4d3c3b0f09beb283e7b19d6
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-10-01 23:16:07 +0530

    basic api in-progress

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
-const app = express();
+const {MongoClient} = require('mongodb');
 
-app.get('/',(req,resp)=>{
-    resp.send("home");
+const app = express();
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectDB(){
+    await client.connect();
+    db = client.db('myDatabaseOne');
+    console.log("mongodb connected!");
+}
+
+connectDB();
+
+// app.set("view engine","ejs");
+app.get('/users', async (req,resp)=>{
+    let result = await db.collection("users").find().toArray();
+    resp.json(result);
 });
+
+app.post('/users',async(req,resp)=>{
+    try {
+        let userData = req.body;
+        let result = await db.collection('users').insertOne(userData);
+        resp.json({message:"Users added"})
+    } catch (error) {
+        resp.status(500).json({error:error.message});
+    }
+})
 
 app.listen(3000);
